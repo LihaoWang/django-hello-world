cced280a9218fe0a9b3d098a0c6bc87cb79f6488
leowang <leowang9863@gmail.com>
2021-10-08 00:58:56 -0400

    added login

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from register import views as v
+from . import views
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path("register/", v.register, name="register"),
-
+    path('', include('register.urls')),
 
 ]
