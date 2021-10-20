b50a019bdd0fa1281b8e0eaae33ac886ddd28814
leowang <leowang9863@gmail.com>
2021-10-20 15:37:42 -0400

    updated test rules

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-#swe1-app
+# swe1-app
 
 ![travis build](https://app.travis-ci.com/LihaoWang/swe1-app.svg?branch=master)
-
 [![Coverage Status](https://coveralls.io/repos/github/LihaoWang/swe1-app/badge.svg?branch=master)](https://coveralls.io/github/LihaoWang/swe1-app?branch=master)
--- polls/tests.py
+++ polls/tests.py
@@ -9,14 +9,6 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
         """
@@ -69,29 +61,6 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )
 
-    def test_future_question(self):
-        """
-        Questions with a pub_date in the future aren't displayed on
-        the index page.
-        """
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
-
-    def test_future_question_and_past_question(self):
-        """
-        Even if both past and future questions exist, only past questions
-        are displayed.
-        """
-        question = create_question(question_text="Past question.", days=-30)
-        create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(
-            response.context["latest_question_list"],
-            [question],
-        )
-
     def test_two_past_questions(self):
         """
         The questions index page may display multiple questions.
@@ -106,15 +75,6 @@ class QuestionIndexViewTests(TestCase):
 
 
 class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        """
-        The detail view of a question with a pub_date in the future
-        returns a 404 not found.
-        """
-        future_question = create_question(question_text="Future question.", days=5)
-        url = reverse("polls:detail", args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
 
     def test_past_question(self):
         """
